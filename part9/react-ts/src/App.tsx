3446f25b703d11e10c08a0014471432b8dd8c290
kapilbamotriya123 <kapilbamotriya12345@gmail.com>
2024-08-13 09:07:58 +0530

    9D theory

--- part9/react-ts/src/App.tsx
+++ part9/react-ts/src/App.tsx
@@ -1,13 +1,50 @@
 
+import { SyntheticEvent, useEffect, useState } from "react"
+import {Note} from '../types'
+import { getAllNotes, createNote} from "./services/notes";
+
 const App = ( ) => {
-  const Hello = ({name}: {name:string}) => {
-    return (
-      `Hello, ${name}`
-    )
+    
+  const [notes, setNotes] = useState<Note[]>([]);
+  const [newNote, setNewNote] = useState('');
+  useEffect( () => {
+    getAllNotes().then(d => {
+      setNotes(d)
+    })
+  }, [])
+
+  const handleSubmit = (e:SyntheticEvent) => {
+    e.preventDefault()
+    createNote({ content: newNote }).then(data => {
+      setNotes(notes.concat(data))
+    })
+    const newNoteWId:Note = {
+      content: newNote,
+      id: Math.max(...notes.map(n => Number(n.id))) + 1,
+    }
+    
+    setNotes(notes.concat(newNoteWId))
+    setNewNote('')
   }
+
+
   return (
     <div>
-      < Hello name = 'typescript' />
+      <h1>
+        Notes
+      </h1>
+      <form onSubmit={handleSubmit}>
+        <input name="note" value={newNote} onChange={(e) => setNewNote(e.target.value)} />
+        <button type="submit" >submit</button>
+      </form>
+
+      <ul>
+        {notes.map(note => (
+          <li key={note.id}>
+            {note.content} 
+          </li>
+        ))}
+      </ul>
     </div>
   )
 }
---
+++ part9/react-ts/types.ts
@@ -0,0 +1,7 @@
+export interface Note {
+    id:number;
+    content: string;
+}
+
+export type NewNote = Omit<Note, 'id'>
+
