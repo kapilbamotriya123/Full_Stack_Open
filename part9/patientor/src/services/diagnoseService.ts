209a597314fc57bae7d0f470490d6f55d5851953
kapilbamotriya123 <kapilbamotriya12345@gmail.com>
2024-08-25 17:15:31 +0530

    completed Patientor app completely and bundled it too with tsc

--- part9/patientor/src/services/diagnoseService.ts
+++
@@ -1,7 +0,0 @@
-import diagnoseEntries from '../../data/diagnoses';
-import { DiagnoseEntry } from '../../types';
-
-export const getDiagnoseEntries = (): DiagnoseEntry[]  => {
-    return diagnoseEntries;
-};
-
